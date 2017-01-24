a101f35452f3b2b997756f34617ab8ea15102bd7
Stanly Johnson <mail@stanlyjohnson.me>
2017-01-24 14:22:22 +0530

    updates

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,4 +1,5 @@
 
+int time_delay = 100;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT); //smart hack to auto-assign the on-board LED
 }
@@ -6,7 +7,7 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  delay(time_delay);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  delay(time_delay);                       // wait for a second
 }
